89ba9d8b98ec98e0a73c4446483d3c1dd7315e62
HOÀNG ĐỨC VINH <20200120@gmail.com>
2021-10-05 16:12:10 +0700

    hoan thanh category

---
+++ assets/js/carousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
